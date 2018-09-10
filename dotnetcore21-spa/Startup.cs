9260cb91b69abf866f5fcc8871a953c2513a7797
Chris Claxton <chrisclaxton@hotmail.co.uk>
2018-09-10 09:29:41 +0100

    updated readme, added env logging

--- dotnetcore21-spa/Startup.cs
+++ dotnetcore21-spa/Startup.cs
@@ -55,8 +55,9 @@ namespace dotnetcore21_spa
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILogger<Startup> logger)
         {
+            logger.LogInformation($"env.EnvironmentName: {env.EnvironmentName}"); //Log the exact name of the environment
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
