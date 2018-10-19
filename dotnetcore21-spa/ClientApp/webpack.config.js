4c81f30af5af3a509c57a41accbaf2d7d9d8221b
Chris Claxton <chrisclaxton@hotmail.co.uk>
2018-10-19 16:45:27 +0100

    updated webpack.config.js to fix bootstrap menu issue

--- dotnetcore21-spa/ClientApp/webpack.config.js
+++ dotnetcore21-spa/ClientApp/webpack.config.js
@@ -22,6 +22,10 @@ const baseUrl = '/';
 
 const cssRules = [
   { loader: 'css-loader' },
+  {
+    loader: 'postcss-loader',
+    options: { plugins: () => [require('autoprefixer')({ browsers: ['last 2 versions'] })]}
+  }
 ];
 
 module.exports = ({production, server, extractCss, coverage, analyze, karma} = {}) => ({
@@ -34,7 +38,8 @@ module.exports = ({production, server, extractCss, coverage, analyze, karma} = {
   },
   entry: {
     app: ['aurelia-bootstrapper'],
-    vendor: ['bluebird'],
+    // required to get jquery and bootstrap to work without having to import it in main.ts
+    vendor: ['bluebird', 'jquery', 'bootstrap'],
   },
   mode: production ? 'production' : 'development',
   output: {
@@ -75,6 +80,8 @@ module.exports = ({production, server, extractCss, coverage, analyze, karma} = {
       { test: /\.tsx?$/, loader: "ts-loader" },
       // use Bluebird as the global Promise implementation:
       { test: /[\/\\]node_modules[\/\\]bluebird[\/\\].+\.js$/, loader: 'expose-loader?Promise' },
+      // exposes jQuery globally as $ and as jQuery:
+      { test: require.resolve('jquery'), loader: 'expose-loader?$!expose-loader?jQuery' },
       // embed small images and fonts as Data Urls and larger ones as files:
       { test: /\.(png|gif|jpg|cur)$/i, loader: 'url-loader', options: { limit: 8192 } },
       { test: /\.woff2(\?v=[0-9]\.[0-9]\.[0-9])?$/i, loader: 'url-loader', options: { limit: 10000, mimetype: 'application/font-woff2' } },
@@ -92,7 +99,10 @@ module.exports = ({production, server, extractCss, coverage, analyze, karma} = {
     ...when(!karma, new DuplicatePackageCheckerPlugin()),
     new AureliaPlugin(),
     new ProvidePlugin({
-      'Promise': 'bluebird'
+      'Promise': 'bluebird',
+      '$': 'jquery',
+      'jQuery': 'jquery',
+      'window.jQuery': 'jquery',
     }),
     new ModuleDependenciesPlugin({
       'aurelia-testing': [ './compile-spy', './view-spy' ]
