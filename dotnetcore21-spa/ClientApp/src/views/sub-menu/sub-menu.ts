513981494f5961a57ae4fe040d19f8cb5113238a
Chris Claxton <chrisclaxton@hotmail.co.uk>
2018-10-19 17:25:43 +0100

    updated the sub-menu to work better inside the main menu

--- dotnetcore21-spa/ClientApp/src/views/sub-menu/sub-menu.ts
+++ dotnetcore21-spa/ClientApp/src/views/sub-menu/sub-menu.ts
@@ -2,7 +2,6 @@ import { Router, RouterConfiguration } from 'aurelia-router';
 import { PLATFORM } from 'aurelia-pal';
 
 export class SubMenu {
-  public heading = 'Child Router';
   router: Router;
   
   configureRouter(config: RouterConfiguration, router: Router) {
